ee078a465fc9704487707676504f9adedb1eb833
Abhitya Krishnaraj <abhitya@gmail.com>
2024-04-15 13:24:46 -0700

    Update arduino_implementation.ino for dist_transform

--- code/arduino/Project/arduino_implementation/arduino_implementation.ino
+++ code/arduino/Project/arduino_implementation/arduino_implementation.ino
@@ -44,8 +44,98 @@ StateMachine machine = StateMachine();
 // Functions
 
 double getDistance(uint16_t input_reading){
-  // TODO for Abhitya
-  return double(input_reading);
+  double dist_transform;
+  if(input_reading > 54528){//xd500
+    dist_transform = 3.5;
+  }
+  else if(input_reading > 36864){//x9000
+    dist_transform = 4.0;
+  }
+  else if(input_reading > 31232){//x7a00
+    dist_transform = 4.5;
+  }
+  else if(input_reading > 26368){//x6700
+    dist_transform = 5.0;
+  }
+  else if(input_reading > 23040){//x5a00
+    dist_transform = 5.5;
+  }
+  else if(input_reading > 19712){//x4d00
+    dist_transform = 6.0;
+  }
+  else if(input_reading > 17152){//x4300
+    dist_transform = 6.5;
+  }
+  else if(input_reading > 14848){//x3a00
+    dist_transform = 7.0;
+  }
+  else if(input_reading > 13312){//x3400
+    dist_transform = 7.5;
+  }
+  else if(input_reading > 12032){//x2f00
+    dist_transform = 8.0;
+  }
+  else if(input_reading > 10752){//x2a00
+    dist_transform = 8.5;
+  }
+  else if(input_reading > 9472){//x2500
+    dist_transform = 9.0;
+  }
+  else if(input_reading > 7936){//x1f00
+    dist_transform = 9.5;
+  }
+  else if(input_reading > 7680){//x1e00
+    dist_transform = 10.0;
+  }
+  else if(input_reading > 7168){//x1c00
+    dist_transform = 10.5;
+  }
+  else if(input_reading > 6400){//x1900
+    dist_transform = 11.0;
+  }
+  else if(input_reading > 5632){//x1600
+    dist_transform = 11.5;
+  }
+  else if(input_reading > 5120){//x1400
+    dist_transform = 12.0;
+  }
+  else if(input_reading > 4608){//x1200
+    dist_transform = 12.5;
+  }
+  else if(input_reading > 4096){//x1000
+    dist_transform = 13.0;
+  }
+  else if(input_reading > 3840){//x0f00
+    dist_transform = 14.0;
+  }
+  else if(input_reading > 3584){//x0e00
+    dist_transform = 15.0;
+  }
+  else if(input_reading > 3072){//x0c00
+    dist_transform = 16.0;
+  }
+  else if(input_reading > 4096){//x0b00
+    dist_transform = 17.0;
+  }
+  else if(input_reading > 3840){//x0a00
+    dist_transform = 18.0;
+  }
+  else if(input_reading > 3584){//x0900
+    dist_transform = 19.0;
+  }
+  else if(input_reading > 3072){//x0800
+    dist_transform = 20.0;
+  }
+  else if(input_reading > 3584){//x0700
+    dist_transform = 21.0;
+  }
+  else if(input_reading > 3072){//x0600
+    dist_transform = 23.0;
+  }
+  else{
+    dist_transform = 25.0;
+  }
+  return dist_transform;
 }
 
 /*convertReading(double reading)
